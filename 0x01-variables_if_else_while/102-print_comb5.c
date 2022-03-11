9a0de20b071174a0ab4c0db50b4abdbca383954f
NegussuKeffeni <nkeffeni@gmail.com>
2022-03-11 18:08:26 +0300

    it is my fix

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -16,20 +16,22 @@ int main(void)
 			{
 				while (x < 58)
 				{
-				    if(p*10+q >= y*10+x)
-				    {
-				    }
-				    else{
-					putchar(p);
-					putchar(q);
-					putchar(' ');
-					putchar(y);
-					putchar(x);
-				    }
+					if (p * 10 + q >= y * 10 + x)
+					{
+					}
+					else
+					{
+						putchar(p);
+						putchar(q);
+						putchar(' ');
+						putchar(y);
+						putchar(x);
+					}
 					if (!(p == 57 &&
-						q == 56 &&
-						y == 57 &&
-						x == 57 ) && !(p*10+q >= y*10+x) )
+						  q == 56 &&
+						  y == 57 &&
+						  x == 57) &&
+						!(p * 10 + q >= y * 10 + x))
 					{
 						putchar(',');
 						putchar(' ');
@@ -37,18 +39,16 @@ int main(void)
 					x++;
 				}
 				y++;
-				x=48;
+				x = 48;
 			}
 			q++;
 			y = 48;
-			x=q;
+			x = q;
 		}
 		p++;
 		q = 48;
 		y = p;
-		x = q+ 1;
-	
-		
+		x = q + 1;
 	}
 	putchar('\n');
 	return (0);
