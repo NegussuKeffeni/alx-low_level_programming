9d7e1f5e325b40d2b6a4e29eeba1735786ac400d
NegussuKeffeni <nkeffeni@gmail.com>
2022-03-11 18:44:09 +0300

    it is my fix

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -6,42 +6,43 @@
  */
 int main(void)
 {
-	int p = 48, q = 48, y = 48, x = 48;
-	for (; p < 58; p++)
-	{
-		for (; q < 58; q++)
-		{
-			for (; y < 58; y++)
-			{
-				for (; x < 58; x++)
-				{
-					if (p * 10 + q >= y * 10 + x)
-					{
-					}
-					else
-					{
-						putchar(p);
-						putchar(q);
-						putchar(' ');
-						putchar(y);
-						putchar(x);
-					}
-					if (!(p == 57 && q == 56 && y == 57 && x == 57) &&
-						!(p * 10 + q >= y * 10 + x))
-					{
-						putchar(',');
-						putchar(' ');
-					}
-				}
-				x = 48;
-			}
-			y = 48;
-			x = q;
-		}
-		q = 48;
-		y = p;
-		x = q + 1;
-	}
-	putchar('\n');
-	return (0);
+int p = 48, q = 48, y = 48, x = 48;
+
+for (; p < 58; p++)
+{
+for (; q < 58; q++)
+{
+for (; y < 58; y++)
+{
+for (; x < 58; x++)
+{
+if (p * 10 + q >= y * 10 + x)
+{
+}
+else
+{
+putchar(p);
+putchar(q);
+putchar(' ');
+putchar(y);
+putchar(x);
+}
+if (!(p == 57 && q == 56 && y == 57 && x == 57) &&
+!(p * 10 + q >= y * 10 + x))
+{
+putchar(',');
+putchar(' ');
+}
+}
+x = 48;
+}
+y = 48;
+x = q;
+}
+q = 48;
+y = p;
+x = q + 1;
+}
+putchar('\n');
+return (0);
 }
