8c8dd04bc9da1c2a34aab58e01f8ad082bd5b475
NegussuKeffeni <nkeffeni@gmail.com>
2022-03-21 17:46:31 +0300

    fix

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -8,12 +8,13 @@
 
 void puts2(char *str)
 {
+	int j = sizeof(str)/sizeof(char);
 	int i = 0;
 
-	while (str[i] != '\0')
+	while ( i <= j)
 	{
 		_putchar(str[i]);
 		_putchar('\n');
-		i=+2;
+		i+=2;
 	}
 }
